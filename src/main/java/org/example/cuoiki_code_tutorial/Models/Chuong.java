2bae4c70fbefeaa3142dc056b3b57cc4702f319c
KietUTE2812 <21110517@student.hcmute.edu.vn>
2024-04-03 23:39:31 +0700

    update Java version

---
+++ src/main/java/org/example/cuoiki_code_tutorial/Models/Chuong.java
@@ -0,0 +1,8 @@
+package org.example.cuoiki_code_tutorial.Models;
+
+public class Chuong {
+    private String maChuong;
+    private String tenChuong;
+    private int thuTu;
+    private String maKH;
+}
