9645878ac4371d7a64e689b2ef9549912d59f321
MrNDN-7 <21110568@student.hcmute.edu.vn>
2024-04-11 17:43:06 +0700

    ducnhan part1

--- src/main/java/org/example/cuoiki_code_tutorial/View/HelloApplication.java
+++ src/main/java/org/example/cuoiki_code_tutorial/View/HelloApplication.java
@@ -1,58 +1,29 @@
 package org.example.cuoiki_code_tutorial.View;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Accordion;
-import javafx.scene.control.Button;
-import javafx.scene.control.TitledPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage primaryStage) {
-        // Tạo một Accordion để chứa các chương
-        Accordion accordion = new Accordion();
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/FXML/login.fxml"));
+        //Scene scene = new Scene(fxmlLoader.load(), 1200, 666);
+        Scene scene = new Scene(fxmlLoader.load(), 800, 630);
+        Image icon = new Image(getClass().getResourceAsStream("/Image/icon.png"));
 
-        // Tạo một VBox chứa danh sách bài học cho mỗi chương
-        VBox chapter1Content = new VBox();
-        chapter1Content.getChildren().addAll(
-                new Button("Bài học 1"),
-                new Button("Bài học 2"),
-                new Button("Bài học 3")
-        );
-        TitledPane chapter1 = new TitledPane("Chương 1", chapter1Content);
+        stage.getIcons().add(icon);
 
-        VBox chapter2Content = new VBox();
-        chapter2Content.getChildren().addAll(
-                new Button("Bài học 1"),
-                new Button("Bài học 2"),
-                new Button("Bài học 3")
-        );
-        TitledPane chapter2 = new TitledPane("Chương 2", chapter2Content);
-
-        // Thêm các chương vào Accordion
-        accordion.getPanes().addAll(chapter1, chapter2);
-
-        // Tạo nút mở rộng
-        Button expandButton = new Button("Expand All");
-        expandButton.setOnAction(event -> {
-            for (TitledPane pane : accordion.getPanes()) {
-                pane.setExpanded(true);
-            }
-        });
-
-        // Tạo layout chính và thêm Accordion và nút mở rộng vào đó
-        VBox root = new VBox(10, expandButton, accordion);
-
-        Scene scene = new Scene(root, 400, 400);
-
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Lesson Viewer");
-        primaryStage.show();
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
-        launch(args);
+        launch();
     }
 }
