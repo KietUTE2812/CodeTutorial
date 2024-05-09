8969019b2227488cb3f4ed049cc8b1af00da97cd
thanhbinh1411 <thanhbinhdang1411@gmail.com>
2024-05-09 17:57:54 +0700

    fix merge

--- src/main/java/org/example/cuoiki_code_tutorial/View/Main.java
+++ src/main/java/org/example/cuoiki_code_tutorial/View/Main.java
@@ -1,31 +1,21 @@
 package org.example.cuoiki_code_tutorial.View;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Main extends Application {
-
     @Override
     public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/FXML/login.fxml"));
+        //Scene scene = new Scene(fxmlLoader.load(), 1200, 666);
+        Scene scene = new Scene(fxmlLoader.load(), 800, 630);
+        Image icon = new Image(getClass().getResourceAsStream("/Image/icon.png"));
 
-        String pathToFXML = "/FXML/lesson_compiler.fxml";
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(pathToFXML));
-
-        AnchorPane root = new AnchorPane();
-        root.getChildren().add(fxmlLoader.load());
-        root.getStyleClass().add("background");
-        Scene scene = new Scene(root, 1000, 600);
-        // Kết nối tệp CSS với scene
-        String pathToStyle = "/CSS/styles_baihoc.css";
-        scene.getStylesheets().add(getClass().getResource(pathToStyle).toExternalForm());
-        stage.setTitle("Code Tutorial");
         stage.setScene(scene);
         stage.show();
     }
